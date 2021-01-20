5ec179c48aa58d03ea7e3874d2d439fb27a76241
Aliaksandr Razumny <61558614+Dev1LtJ@users.noreply.github.com>
2021-01-20 16:58:32 +0300

    Added Edit and Delete function

--- js/script.js
+++ js/script.js
@@ -55,18 +55,26 @@ function moveToggle(value) {
     }
 }
 
-//create tasks
-let addTaskBtn = document.querySelector('button[type="submit"]'),
+//finding elements
+let submitTaskBtn = document.querySelector('button[type="submit"]'),
+    editTaskBtn = submitTaskBtn.cloneNode(true),
+    addTaskBtn = document.getElementById('addTask'),
     taskList = document.getElementById('currentTasks'),
     completedTaskList = document.getElementById('completedTasks'),
     taskElem = document.querySelector('.list-group-item'),
     closeBtn = document.querySelector('.close'),
-    dropdownMenu = document.querySelector('.dropdown-menu'),
+    dropdownMenu = document.querySelector('.dropdown-menu'), //Где-то используется?
     titleForm = document.getElementById('inputTitle'),
     textForm = document.getElementById('inputText'),
     radios = document.querySelectorAll('.form-check-input'),
+//creating arrays
     tasks = [],
     completedTasks = [];
+
+//initial properties
+    editTaskBtn.textContent = 'Edit task';
+    submitTaskBtn.parentElement.append(editTaskBtn);
+    editTaskBtn.hidden = true;
 
 class Task {
     constructor(name, priority) {
@@ -75,7 +83,7 @@ class Task {
     title = titleForm.value;
     text = textForm.value;
     priority = this.checkPriority(radios);
-    time = new Date();
+    time = this.getTime(new Date());
     status = 'inProgress';
     editable = 'true';
     checkPriority(radios) {
@@ -83,16 +91,36 @@ class Task {
             if (radio.checked === true) return radio.value;
         }
     };
+    getTime(timeStamp) {
+        let hours = timeStamp.getHours(),
+            date = timeStamp.getDate(),
+            month = timeStamp.getMonth() + 1,
+            year = timeStamp.getFullYear(),
+            minutes = timeStamp.getMinutes();
+        (month > 0 && month < 10) ? month = '0' + month : month = month;
+        (minutes > 0 && minutes < 10) ? minutes = '0' + minutes : minutes = minutes;
+        return `${hours}:${minutes} ${date}.${month}.${year}`;
+    }
 }
 
 //create task
-addTaskBtn.addEventListener('click', (event) => {
+submitTaskBtn.addEventListener('click', function addTask(event) {
     event.preventDefault();
     tasks.push(new Task(String(tasks.length)));
     closeBtn.dispatchEvent(new Event ('click', {bubbles : true}));
     renderDom(taskList, tasks);
+    clearForm();
 });
 
+function clearForm() {
+    titleForm.value = '';
+    textForm.value = '';
+    for (let radio of radios) {
+        radio.checked = false;
+    }
+}
+
+//renderDom
 function renderDom(nodeList, tasksArray) {
     clearPreviousDom(nodeList);
     tasksArray.forEach((item) => {
@@ -100,45 +128,37 @@ function renderDom(nodeList, tasksArray) {
         taskElemCopy.setAttribute('id', item.name);
         taskElemCopy.querySelector('.title').textContent = item.title;
         taskElemCopy.querySelector('.text').textContent = item.text;
-        taskElemCopy.querySelector('.time').textContent = getTime(item.time);
+        taskElemCopy.querySelector('.time').textContent = item.time;
         taskElemCopy.querySelector('.priority').textContent = item.priority + ' priority';
         nodeList.append(taskElemCopy);
     });
 }
-
 function clearPreviousDom(nodeList) {
     while (nodeList.firstChild) {
         nodeList.firstChild.remove();
     }
 }
 
-function getTime(timeStamp) {
-    let hours = timeStamp.getHours(),
-        date = timeStamp.getDate(),
-        month = timeStamp.getMonth() + 1,
-        year = timeStamp.getFullYear(),
-        minutes = timeStamp.getMinutes();
-
-    (month > 0 && month < 10) ? month = '0' + month : month = month;
-    (minutes > 0 && minutes < 10) ? minutes = '0' + minutes : minutes = minutes;
-
-    return `${hours}:${minutes} ${date}.${month}.${year}`;
-}
-
 //delete, complete, edit task event delegation
 taskList.addEventListener('click', (event) => {
+    let taskName = event.target.closest('.list-group-item').id;
     if (event.target.classList.contains('btn-success')) {
-        let taskName = event.target.closest('.list-group-item').id;
         completeTask(taskName);
         renderDom(completedTaskList, completedTasks);
         renderDom(taskList, tasks);
     } else if (event.target.classList.contains('btn-info')) {
-        console.log('lol');
+        clearForm(); //подумать над ее расположением, почему работает только тут?
+        editTask(taskName);
+        renderDom(taskList, tasks);
     } else if (event.target.classList.contains('btn-danger')) {
-        console.log('arbidol');
+        deleteTask(taskName);
+        renderDom(taskList, tasks);
+    } else {
+        return false;
     }
 });
 
+//complete task
 function completeTask (taskName) {
     let taskPosition  = tasks.findIndex((item) => item.name == taskName);
     if (taskPosition == (tasks.length - 1)) {
@@ -152,3 +172,56 @@ function completeTask (taskName) {
         returnableTasks.length = 0;
     }
 }
+//delete task
+function deleteTask (taskName) {
+    let taskPosition  = tasks.findIndex((item) => item.name == taskName);
+    if (taskPosition == (tasks.length - 1)) {
+        let completedTask = tasks.splice(taskPosition, tasks.length - taskPosition)[0];
+    } else {
+        let returnableTasks = tasks.slice(taskPosition + 1);
+        let completedTask = tasks.splice(taskPosition, tasks.length - taskPosition)[0]; 
+        returnableTasks.forEach((item) => tasks.push(item));
+        returnableTasks.length = 0;
+    }
+}
+//edit task
+function editTask(taskName) {
+    addTaskBtn.dispatchEvent(new Event ('click', {bubbles : true}));
+    submitTaskBtn.hidden = true;
+    editTaskBtn.hidden = false;
+    let taskPosition  = tasks.findIndex((item) => item.name == taskName);
+    console.log(taskPosition);
+    titleForm.value = tasks[taskPosition].title;
+    textForm.value = tasks[taskPosition].text;
+    for (let radio of radios) {
+        if (radio.value == tasks[taskPosition].priority) radio.checked = true;
+    }
+    editTaskBtn.addEventListener('click', function editTask(event) {
+        event.preventDefault();
+        tasks[taskPosition].title = titleForm.value;
+        tasks[taskPosition].text = textForm.value;
+        tasks[taskPosition].priority = tasks[taskPosition].checkPriority(radios);
+        submitTaskBtn.hidden = false;
+        editTaskBtn.hidden = true;
+        closeBtn.dispatchEvent(new Event ('click', {bubbles : true}));
+        renderDom(taskList, tasks);
+    });
+}
+
+// completedTaskList.addEventListener('click', (event) => {
+//     let taskName = event.target.closest('.list-group-item').id;
+//     if (event.target.classList.contains('btn-success')) {
+//         completeTask(taskName);
+//         renderDom(completedTaskList, completedTasks);
+//         renderDom(taskList, tasks);
+//     } else if (event.target.classList.contains('btn-info')) {
+//         clearForm(); //подумать над ее расположением, почему работает только тут?
+//         editTask(taskName);
+//         renderDom(taskList, tasks);
+//     } else if (event.target.classList.contains('btn-danger')) {
+//         deleteTask(taskName);
+//         renderDom(taskList, tasks);
+//     } else {
+//         return false;
+//     }
+// });
