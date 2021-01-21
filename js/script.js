71215bb3f38f95323816b205c533a9f8ff4e05eb
Aliaksandr Razumny <61558614+Dev1LtJ@users.noreply.github.com>
2021-01-21 14:32:02 +0300

    Optimization features

--- js/script.js
+++ js/script.js
@@ -1,61 +1,3 @@
-//toggle theme
-let body = document.body,
-    settings = document.querySelector('.dropdown-menu'),
-    toggle = document.createElement('div'),
-    toggle__btn = document.createElement('div'),
-    toggle__descr = document.createElement('div'),
-    toggle__img = document.createElement('img');
-//toggler creation
-settings.innerHTML = ('');
-toggle.classList.add('toggle');
-toggle__btn.classList.add('toggle__btn');
-toggle__descr.classList.add('toggle__descr');
-toggle__img.classList.add('toggle__img');
-toggle__img.setAttribute('src', 'icons/sun.svg');
-toggle__descr.innerHTML = 'Theme'
-toggle__btn.append(toggle__img);
-toggle.append(toggle__btn);
-toggle.append(toggle__descr);
-settings.append(toggle);
-//toggler logic
-toggle__btn.addEventListener('click', (event) => {
-    event.stopPropagation();
-    body.classList.toggle('darktheme');
-    if (body.classList.contains('darktheme')) {
-        toggle__img.setAttribute('src', 'icons/moon.svg');
-        moveToggle(true);
-    } else {
-        toggle__img.setAttribute('src', 'icons/sun.svg');
-        moveToggle(false);
-    }
-});
-function moveToggle(value) {
-    let left = null;
-    value ? left = 0 : left = 22;
-    if (value) {
-        let timer = setTimeout(function run() {
-            if (toggle__img.style.left === '22px') {
-                clearTimeout(timer);
-            } else {
-                toggle__img.style.left = left + 'px';
-                left++;
-                setTimeout(run, 10);
-            }
-        }, 10);
-    } else {
-        let timer = setTimeout(function run() {
-            if (toggle__img.style.left === '0px') {
-                clearTimeout(timer);
-            } else {
-                toggle__img.style.left = left + 'px';
-                left--;
-                setTimeout(run, 10);
-            }
-        }, 10);
-    }
-}
-
-//finding elements
 let submitTaskBtn = document.querySelector('button[type="submit"]'),
     editTaskBtn = submitTaskBtn.cloneNode(true),
     addTaskBtn = document.getElementById('addTask'),
@@ -63,11 +5,9 @@ let submitTaskBtn = document.querySelector('button[type="submit"]'),
     completedTaskList = document.getElementById('completedTasks'),
     taskElem = document.querySelector('.list-group-item'),
     closeBtn = document.querySelector('.close'),
-    dropdownMenu = document.querySelector('.dropdown-menu'), //Где-то используется?
     titleForm = document.getElementById('inputTitle'),
     textForm = document.getElementById('inputText'),
     radios = document.querySelectorAll('.form-check-input'),
-//creating arrays
     tasks = [],
     completedTasks = [];
 
@@ -77,15 +17,13 @@ let submitTaskBtn = document.querySelector('button[type="submit"]'),
     editTaskBtn.hidden = true;
 
 class Task {
-    constructor(name, priority) {
-        this.name = name;
+    constructor(id) {
+        this.id = id;
     }
     title = titleForm.value;
     text = textForm.value;
     priority = this.checkPriority(radios);
     time = this.getTime(new Date());
-    status = 'inProgress';
-    editable = 'true';
     checkPriority(radios) {
         for (let radio of radios) {
             if (radio.checked === true) return radio.value;
@@ -106,7 +44,7 @@ class Task {
 //create task
 submitTaskBtn.addEventListener('click', function addTask(event) {
     event.preventDefault();
-    tasks.push(new Task(String(tasks.length)));
+    tasks.push(new Task((tasks.length)));
     closeBtn.dispatchEvent(new Event ('click', {bubbles : true}));
     renderDom(taskList, tasks);
     clearForm();
@@ -125,7 +63,7 @@ function renderDom(nodeList, tasksArray) {
     clearPreviousDom(nodeList);
     tasksArray.forEach((item) => {
         let taskElemCopy = taskElem.cloneNode(true);
-        taskElemCopy.setAttribute('id', item.name);
+        taskElemCopy.setAttribute('id', item.id);
         taskElemCopy.querySelector('.title').textContent = item.title;
         taskElemCopy.querySelector('.text').textContent = item.text;
         taskElemCopy.querySelector('.time').textContent = item.time;
@@ -141,17 +79,17 @@ function clearPreviousDom(nodeList) {
 
 //delete, complete, edit task event delegation
 taskList.addEventListener('click', (event) => {
-    let taskName = event.target.closest('.list-group-item').id;
+    let taskId = event.target.closest('.list-group-item').id;
     if (event.target.classList.contains('btn-success')) {
-        completeTask(taskName);
+        completeTask(taskId);
         renderDom(completedTaskList, completedTasks);
         renderDom(taskList, tasks);
     } else if (event.target.classList.contains('btn-info')) {
         clearForm(); //подумать над ее расположением, почему работает только тут?
-        editTask(taskName);
+        editTask(taskId);
         renderDom(taskList, tasks);
     } else if (event.target.classList.contains('btn-danger')) {
-        deleteTask(taskName);
+        deleteTask(taskId);
         renderDom(taskList, tasks);
     } else {
         return false;
@@ -159,8 +97,8 @@ taskList.addEventListener('click', (event) => {
 });
 
 //complete task
-function completeTask (taskName) {
-    let taskPosition  = tasks.findIndex((item) => item.name == taskName);
+function completeTask (taskId) {
+    let taskPosition  = tasks.findIndex((item) => item.id == taskId);
     if (taskPosition == (tasks.length - 1)) {
         let completedTask = tasks.splice(taskPosition, tasks.length - taskPosition)[0];
         completedTasks.push(completedTask);
@@ -173,8 +111,8 @@ function completeTask (taskName) {
     }
 }
 //delete task
-function deleteTask (taskName) {
-    let taskPosition  = tasks.findIndex((item) => item.name == taskName);
+function deleteTask (taskId) {
+    let taskPosition  = tasks.findIndex((item) => item.id == taskId);
     if (taskPosition == (tasks.length - 1)) {
         let completedTask = tasks.splice(taskPosition, tasks.length - taskPosition)[0];
     } else {
@@ -185,12 +123,11 @@ function deleteTask (taskName) {
     }
 }
 //edit task
-function editTask(taskName) {
+function editTask(taskId) {
     addTaskBtn.dispatchEvent(new Event ('click', {bubbles : true}));
     submitTaskBtn.hidden = true;
     editTaskBtn.hidden = false;
-    let taskPosition  = tasks.findIndex((item) => item.name == taskName);
-    console.log(taskPosition);
+    let taskPosition  = tasks.findIndex((item) => item.id == taskId);
     titleForm.value = tasks[taskPosition].title;
     textForm.value = tasks[taskPosition].text;
     for (let radio of radios) {
---
+++ js/theme-toggler.js
@@ -0,0 +1,56 @@
+//toggle theme
+let body = document.body,
+    settings = document.querySelector('.dropdown-menu'),
+    toggle = document.createElement('div'),
+    toggle__btn = document.createElement('div'),
+    toggle__descr = document.createElement('div'),
+    toggle__img = document.createElement('img');
+//toggler creation
+settings.innerHTML = ('');
+toggle.classList.add('toggle');
+toggle__btn.classList.add('toggle__btn');
+toggle__descr.classList.add('toggle__descr');
+toggle__img.classList.add('toggle__img');
+toggle__img.setAttribute('src', 'icons/sun.svg');
+toggle__descr.innerHTML = 'Theme'
+toggle__btn.append(toggle__img);
+toggle.append(toggle__btn);
+toggle.append(toggle__descr);
+settings.append(toggle);
+//toggler logic
+toggle__btn.addEventListener('click', (event) => {
+    event.stopPropagation();
+    body.classList.toggle('darktheme');
+    if (body.classList.contains('darktheme')) {
+        toggle__img.setAttribute('src', 'icons/moon.svg');
+        moveToggle(true);
+    } else {
+        toggle__img.setAttribute('src', 'icons/sun.svg');
+        moveToggle(false);
+    }
+});
+function moveToggle(value) {
+    let left = null;
+    value ? left = 0 : left = 22;
+    if (value) {
+        let timer = setTimeout(function run() {
+            if (toggle__img.style.left === '22px') {
+                clearTimeout(timer);
+            } else {
+                toggle__img.style.left = left + 'px';
+                left++;
+                setTimeout(run, 10);
+            }
+        }, 10);
+    } else {
+        let timer = setTimeout(function run() {
+            if (toggle__img.style.left === '0px') {
+                clearTimeout(timer);
+            } else {
+                toggle__img.style.left = left + 'px';
+                left--;
+                setTimeout(run, 10);
+            }
+        }, 10);
+    }
+}
