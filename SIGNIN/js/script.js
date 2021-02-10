0b94950ca8c39bae1c7d2433a3b653a5aed934b0
Aliaksandr Razumny <61558614+Dev1LtJ@users.noreply.github.com>
2021-02-10 16:48:36 +0300

    Delete SIGNIN directory

--- SIGNIN/js/script.js
+++
@@ -1,5 +0,0 @@
-let emailCheck = document.querySelector('.auth-form__email-check'),
-    passwordCheck = document.querySelector('.auth-form__password-check'),
-    email = document.getElementById('email'),
-    password = document.getElementById('password'),
-    submitButton = document.querySelector('.auth-form__signin');
