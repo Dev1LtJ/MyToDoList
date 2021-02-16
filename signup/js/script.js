c5bbf016853078690bf9c0df2fcc290a92a7ef9d
Aliaksandr Razumny <61558614+Dev1LtJ@users.noreply.github.com>
2021-02-16 16:51:01 +0300

    Form validation

--- signup/js/script.js
+++ signup/js/script.js
@@ -1,6 +1,8 @@
 //elements
 let passwordBar = document.querySelector('.auth-form__divider'),
     passwordCheck = document.querySelector('.auth-form__password-check'),
+    loginCheck = document.querySelector('.auth-form__login-check'),
+    emailCheck = document.querySelector('.auth-form__email-check'),
     username = document.getElementById('name'),
     surname = document.getElementById('surname'),
     login = document.getElementById('login'),
@@ -17,7 +19,8 @@ const
     COLOR_BLUE_VISIBLE = 'rgba(88, 134, 27, 1)',
     COLOR_BLUE_INVISIBLE = 'rgba(88, 134, 27, 0)',
     COLOR_INCORRECT = '#fd1000',
-    COLOR_CORRENT = '#7dbf26';
+    COLOR_CORRENT = '#7dbf26',
+    COLOR_DEFAULT = '#000';
 
 import {setToLocalStorage,
         getFromLocalStorage} from '../../MyToDoList/js/localStorage.js';
@@ -33,7 +36,7 @@ class User {
         this.name = name;
     }
     surname = surname.value;
-    login = login.value;
+    login = login.value.toLowerCase();
     email = email.value.toLowerCase();
     password = password.value;
     tasks = [];
@@ -41,6 +44,9 @@ class User {
 
 submitButton.addEventListener('click', (event)=> {
     event.preventDefault();
+    if (!checkLogin(login.value.toLowerCase()) ||
+        !checkEmail(email.value.toLowerCase()) ||
+        password.value != repeatedPassword.value) return false;
     let newUser = new User(username.value);
     users.push(newUser);
     setToLocalStorage(users, 'users');
@@ -75,3 +81,62 @@ function checkPassword (baseElement, trackingElement) {
         passwordCheck.style.color = COLOR_CORRENT;
     }
 }
+
+function checkLogin (value) {
+    for (let user of users) {
+        if(user.login === value) {
+            loginCheck.textContent = 'User with this login is already registered';
+            loginCheck.style.color = COLOR_INCORRECT;
+            return false;
+        }
+    }
+    return true;
+}
+
+login.addEventListener('input', () => {
+    loginCheck.textContent = 'Login matching checker';
+    loginCheck.style.color = COLOR_DEFAULT;
+});
+
+function checkEmail (value) {
+    let counter = 0;
+    let newPos = 0;
+    let foundPos = 0;
+    while (true) {
+        foundPos = value.indexOf('@', newPos);
+        if (foundPos == -1) break;
+        counter++;
+        newPos = foundPos + 1;
+    }
+    if (counter != 1) {
+        emailCheck.textContent = 'Email entered is incorrect';
+        emailCheck.style.color = COLOR_INCORRECT;
+        return false;
+    }
+    let dog = newPos;
+    counter = 0;
+    while (true) {
+        foundPos = value.indexOf('.', newPos);
+        if (foundPos == -1) break;
+        counter++;
+        newPos = foundPos + 1;
+    }
+    if (counter != 1 || ++dog === newPos) {
+        emailCheck.textContent = 'Email entered is incorrect';
+        emailCheck.style.color = COLOR_INCORRECT;
+        return false;
+    }
+    for (let user of users) {
+        if(user.email === value) {
+            emailCheck.textContent = 'User with this email is already registered';
+            emailCheck.style.color = COLOR_INCORRECT;
+            return false;
+        }
+    }
+    return true;
+}
+
+email.addEventListener('input', () => {
+    emailCheck.textContent = 'Email matching checker';
+    emailCheck.style.color = COLOR_DEFAULT;
+});
